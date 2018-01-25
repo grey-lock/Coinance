a14a4a71088bff8005c8e33cec3cb051b87ae949
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-25 22:31:06 +0000

    Add nested routes for user, add resource links to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :coins
-  resources :transactions
-  resources :wallets
-  root 'home#home'
-  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :coins
+  
+  # Use nested resource so only current_user can view their wallets, transactions
+  resources :users, only: [:show] do
+    resources :transactions
+    resources :wallets
+  end
+  
+  root 'home#home'
+ 
   
 end
