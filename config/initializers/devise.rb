061898ad5c7e381e354c2b72ad2c5577b6d450d7
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-25 20:26:37 +0000

    FB Omniauth works

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,7 +252,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'user,public_repo'
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
