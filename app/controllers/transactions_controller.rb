3a957c03379532291708cf72f8ab7e869c46bc6a
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-25 21:57:15 +0000

    Add tx controller index, show logic

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,18 @@
 class TransactionsController < ApplicationController
+  before_action :set_user_tx
+  
+  def index
+    @transactions = current_user.transactions
+  end
+  
+  def show
+    
+  end
+  
+  private
+  
+  def set_user_tx
+    @transaction = Transaction.find_by(id: params[:id])
+  end
+  
 end
