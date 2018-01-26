77fc760eb4befa8abebef65a46fac452b2f243c6
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-26 01:38:58 +0000

    Add coin list method and list to index

--- app/models/cryptocompare_api.rb
+++ app/models/cryptocompare_api.rb
@@ -4,4 +4,8 @@ class CryptocompareApi < ApplicationRecord
     Cryptocompare::CoinSnapshot.find("#{crypto.upcase}","#{fiat.upcase}")['Data'].values[6]['PRICE'].to_f.round(2)
   end
   
+  def self.list_all_coin_symbols
+    Cryptocompare::CoinList.all['Data'].keys
+  end
+  
 end
