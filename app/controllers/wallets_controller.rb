48443d5393f54a01e2eacaaf63960789982e69f6
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-01 19:04:49 +0000

    Add new wallet action/params, add placeholder for create wallet, add form partial file

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,15 @@
 class WalletsController < ApplicationController
+  
+  def new
+    @wallet = Wallet.new
+  end
+  
+  private
+  
+  # Require wallet params, as well as user_id & coin_id
+  def wallet_params
+    params.require(:wallet).permit(:name, :coin_amount, :user_deposit, :net_value, user_ids: [], coin_ids:[])
+  end
+  
+  
 end
