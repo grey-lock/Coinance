4f4642dd0bf932660ee04ed95dd867bdaa574c54
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-02 19:53:49 +0000

    Add hidden field in form partials

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -13,7 +13,8 @@ class TransactionsController < ApplicationController
   end
   
   def create
-  
+    @transaction = current_user.transactions.build(transaction_params)
+    
   end
   
   private
