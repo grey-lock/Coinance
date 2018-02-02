1f75a39229482352b69d525e983a75cb8539cd25
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-02 18:06:32 +0000

    Make transaction belong_to wallet, coin optional

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  belongs_to :coin
-  belongs_to :wallet
+  belongs_to :coin, optional: true
+  belongs_to :wallet, optional: true
   
   validates :amount, :quantity, :price_per_coin, :fee, numericality: { greater_than: 0 }
   
