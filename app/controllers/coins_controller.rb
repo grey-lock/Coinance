4dece8f637595085de5bb75ba374cc776fa87d71
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-05 22:38:56 +0000

    Move most_tx page to index action

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -2,10 +2,11 @@ class CoinsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    
+     @most_transactions = Coin.most_transactions
   end
   
   def show
-    @most_transactions = Coin.most_transactions
+   
   end
+ 
 end
