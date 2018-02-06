b7a5f123ed9d592d497bde0a29025a579331a354
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-06 22:13:27 +0000

    Add edit/delete buttons to tx show view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -24,10 +24,11 @@ class TransactionsController < ApplicationController
     coin_name = coin_info[0...-1].join(" ")
     coin_symbol = coin_info[-1].scan(/\w+(?!\w|\()/)[0]
     
+    # Assign the params to the coin
     @transaction.coin.name = coin_name
     @transaction.coin.symbol = coin_symbol
     coin_price = CryptocompareApi.last_known_value(coin_symbol) if params[:transaction][:coin]
-    
+  
     @transaction.coin.last_known_value = coin_price if coin_price
     
     if @transaction.valid?
