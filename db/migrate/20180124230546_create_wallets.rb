dea0196f3a0ad4cb0e1891b2712155844dad3715
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-07 02:25:04 +0000

    Able to add some precision to forms, update does not work

--- db/migrate/20180124230546_create_wallets.rb
+++ db/migrate/20180124230546_create_wallets.rb
@@ -2,9 +2,9 @@ class CreateWallets < ActiveRecord::Migration[5.1]
   def change
     create_table :wallets do |t|
       t.string :name
-      t.decimal :coin_amount, default: 0.0000, precision: 12, scale: 6
-      t.decimal :user_deposit, default: 0.0000, precision: 12, scale: 6
-      t.decimal :net_value, default: 0.0000, precision: 12, scale: 6
+      t.float :coin_amount, default: 0.0000, precision: 12, scale: 6
+      t.float :user_deposit, default: 0.0000, precision: 12, scale: 6
+      t.float :net_value, default: 0.0000, precision: 12, scale: 6
       t.belongs_to :user
       t.belongs_to :coin
 
--- db/migrate/20180124231425_create_transactions.rb
+++ db/migrate/20180124231425_create_transactions.rb
@@ -1,10 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
-      t.decimal :amount, default: 0.0000, precision: 12, scale: 6
-      t.decimal :quantity, default: 0.0000, precision: 12, scale: 6
-      t.decimal :price_per_coin, default: 0.0000, precision: 12, scale: 6
-      t.decimal :fee, default: 0.0000, precision: 12, scale: 6
+      t.float :amount, default: 0.0000, precision: 12, scale: 6
+      t.float :quantity, default: 0.0000, precision: 12, scale: 6
+      t.float :price_per_coin, default: 0.0000, precision: 12, scale: 6
+      t.float :fee, default: 0.0000, precision: 12, scale: 6
       t.belongs_to :user
       t.belongs_to :coin
       
