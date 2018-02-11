6cc8b34b2903e1d18c0763c17a41ee6fd77fcd87
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-11 19:02:23 +0000

    Change precision of form values to 0.0000

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -6,7 +6,7 @@ class Wallet < ApplicationRecord
   # has_many :transactions
   
   validates :name, presence: true
-  validates :coin_amount, :user_deposit, format: { with: /\A\d{1,4}(.\d{0,2})?\z/  }, numericality: { greater_than_or_equal_to: 0.00001 }
+  validates :coin_amount, :user_deposit, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
   
   def wallet_params=(wallet_params)
     @wallet_params = wallet_params
