7b9c3f9867d6658e9254aa66eb9cdb1f547f452b
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-12 20:48:17 +0000

    Add wallet and transation association and stub out wallet attribute method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,14 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :coin
-  
-  # Removing this association until app is more complete
-  # belongs_to :wallet, optional: true
+  belongs_to :wallet
   
   validates :amount, :quantity, :price_per_coin, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
   validates :fee, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
+  
+  def wallet_attributes=(wallet_attributes)
+    
+  end
   
   def transaction_params=(transaction_params)
     @transaction_params = transaction_params
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,9 +1,7 @@
 class Wallet < ApplicationRecord
   belongs_to :user
   belongs_to :coin
-  
-  # Removing this association for now due to complexity of models
-  # has_many :transactions
+  has_many :transactions
   
   validates :name, presence: true
   validates :coin_amount, :user_deposit, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
