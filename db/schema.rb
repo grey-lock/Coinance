0fdcb338f55209314e1a16ae0fbdc6176a5a751a
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-13 04:41:36 +0000

    Add note model which belongs to a tx

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124232110) do
+ActiveRecord::Schema.define(version: 20180213043708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180124232110) do
     t.string "symbol"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "transaction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["transaction_id"], name: "index_notes_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
