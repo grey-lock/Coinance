58b39391e24e134e46f1d79a5d3fd9ac788c1d69
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-13 04:34:52 +0000

    Remove tx/wallet association for now

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :coin
-  belongs_to :wallet, optional: true
+  # belongs_to :wallet, optional: true
   
   validates :amount, :quantity, :price_per_coin, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
   validates :fee, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
@@ -10,8 +10,6 @@ class Transaction < ApplicationRecord
     wallets_attributes.values.each do |wall_attr|
       self.wallets.build(wall_attr)
     end
-    # self.build_wallet(wallet_attributes)
-    # self.wallet_id = Wallet.find(id: wallet_attributes[:id])
   end
   
   def transaction_params=(transaction_params)
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,7 +1,7 @@
 class Wallet < ApplicationRecord
   belongs_to :user
   belongs_to :coin
-  has_many :transactions
+  # has_many :transactions
   
   validates :name, presence: true
   validates :coin_amount, :user_deposit, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
