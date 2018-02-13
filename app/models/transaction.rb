070d71482df332b47a7f68cf6da5708ecb71115c
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-13 04:43:00 +0000

    Add association to tx

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :coin
+  has_many :notes
   # belongs_to :wallet, optional: true
   
   validates :amount, :quantity, :price_per_coin, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
