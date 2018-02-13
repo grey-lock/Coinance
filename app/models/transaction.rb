3f45989371a321efae55ec0a288024d951edaa27
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-13 04:44:57 +0000

    Add notes attributes to tx model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,6 +7,14 @@ class Transaction < ApplicationRecord
   validates :amount, :quantity, :price_per_coin, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
   validates :fee, format: { with: /\A\d{1,6}(.\d{0,4})?\z/  }, numericality: { greater_than_or_equal_to: 0.0000 }
   
+  def note_attributes=(note_attributes)
+    note_attributes.values.each do |comment|
+      if !comment.empty? 
+        self.notes.build(comment: comment, tx: self)
+      end
+    end
+  end
+  
   def wallets_attributes=(wallets_attributes)
     wallets_attributes.values.each do |wall_attr|
       self.wallets.build(wall_attr)
