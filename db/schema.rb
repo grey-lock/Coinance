3a8c569cce69a06c7a052f295ee8d72ded9cc00d
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-28 23:27:00 +0000

    Edit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215184651) do
+ActiveRecord::Schema.define(version: 20180228231715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180215184651) do
     t.string "symbol"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "note"
+    t.bigint "transaction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["transaction_id"], name: "index_comments_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
