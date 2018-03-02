50ba0d5b7a976ddca4644eb45fb00bc2607db352
Janusz Szubert <theinvalidnonce@gmail.com>
2018-03-02 20:34:44 +0000

    Add tx js function object properties

---
+++ app/assets/javascripts/transactions.js
@@ -0,0 +1,13 @@
+// An important difference between function declarations and class declarations is that 
+// function declarations are hoisted and class declarations are not.
+
+function Transaction(props) {
+  this.id = props.id
+  this.user_id = props.user_id
+  this.wallet_id = props.wallet_id
+  this.coin_id = props.coin_id
+  this.amount = props.amount
+  this.quantity = props.quantity
+  this.price_per_coin = props.price_per_coin
+  this.fee = props.fee
+}
