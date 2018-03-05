cef48a5f82e8ca3959dd69c2eb30ce76c9f49808
Janusz Szubert <theinvalidnonce@gmail.com>
2018-03-05 00:24:15 +0000

    Add wallet list tx template, add more properties to tx and wallet js templates

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,6 @@
 class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :amount, :quantity, :price_per_coin, :fee, :wallet_id, :coin_id, :created_at, :updated_at
   has_many :comments
+  belongs_to :coin
   
 end
